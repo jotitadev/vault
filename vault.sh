9309f53f76fec04b5e0b090c9fc9b9881e2bb0c7
jorge alvarezcAAexot <jalvarez@redbird.com.ar>
2024-12-02 09:33:02 -0300

    final

--- vault.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-UNSEAL_KEY="Q0dqdU6ApQvAg+srRmrZliF3pinmm6whxlftO1Truro="
-NONCE="7b78a78d-ee0d-b5a3-9982-f45470362c9d"
-OTP="wYNGxXuqC8MIsSdR28Ut0ZqnCMP0"
-
-echo "$UNSEAL_KEY" | kubectl exec -n vault -i vault-0 -- vault operator generate-root -nonce="$NONCE" -otp="$OTP" -
